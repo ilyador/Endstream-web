bbf7fca92529423ff6ad449ae19f1efd34d8aebd
Ilya Dorman <whitestar.i8@gmail.com>
2020-04-29 20:47:59 +0100

    board basic arrangement

--- src/Board.js
+++ src/Board.js
@@ -1,39 +1,74 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import { createUseStyles, useTheme } from 'react-jss'
 import board from './game-data/board.json'
+import game from './game-data/new-game.js'
 import { illustrations } from './load-images'
 
 
-const useStyles = createUseStyles(theme => ({
-  board: {
+const IDs = {
+  me: 'player1',
+  opponent: 'player2',
+  outerworld: 'outerworld'
+}
+const { epochs, streams: _streams } = board
+const { streams } = game
 
-  },
+
+const useStyles = createUseStyles((theme) => ({
+  board: {},
   stream: {
     display: 'flex'
   },
-  epoch: {
-    border: '1px solid gray'
+  me: {
+    extend: 'stream'
   },
+  opponent: {
+    extend: 'stream'
+  },
+  outerworld: {
+    extend: 'stream',
+  },
+  epoch: {
+    flex: 1,
+    border: '1px solid gray'
+  }
 }))
 
 
-const { epochs, streams } = board
-
-
 export default function Board () {
+
   const theme = useTheme()
   const c = useStyles({ theme })
 
+
   return (
     <div className={c.board}>
-      {/*<img src={illustrations['big/Mori-the-Piercer']} alt="Mori" />*/}
-      {streams.map(stream =>
-        <div className={c.stream} key={stream}>
-          {epochs.map(epoch =>
-            <div className={c.epoch} key={epoch}>{epoch}</div>
-          )}
-        </div>
-      )}
+      {_streams.map(stream => {
+        let playerStream = streams[IDs[stream]]
+
+        return (
+          <div className={c[stream]} key={stream}>
+            {epochs.map(epoch => {
+              let _epoch = playerStream[epoch]
+              let operators = _epoch && _epoch.operators
+              let hideouts = _epoch && _epoch.hideouts
+
+              return (
+                <div className={c.epoch} key={epoch}>
+                  {operators && operators.map(operator =>
+                    <div key={operator}>{operator[1]}</div>
+                  )}
+                  {hideouts && hideouts.map(hideouts =>
+                    <div key={hideouts}>{hideouts[1]}</div>
+                  )}
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }
+
+// <img src={illustrations[`big/${id}`]} alt={id}} />
---
+++ src/game-data/new-game.js
@@ -0,0 +1,110 @@
+export default {
+  gameId: 'hv34t3',
+  streams: {
+    player1: {
+      1800: {
+        agenda: 'ms',
+        operators: [
+          [
+            'player1',
+            'Mori-the-Piercer'
+          ], [
+            'player1',
+            'Ben-Storm'
+          ]
+        ]
+      },
+      1900: {
+        agenda: 'mp',
+        operators: [
+          [
+            'player1',
+            'Disruptor'
+          ], [
+            'player2',
+            'Dr-Koh'
+          ]
+        ]
+      },
+      2000: {
+        agenda: 'pm'
+      },
+      2100: {
+        agenda: 'ps',
+        hideouts: [
+          [
+            'player1',
+            'SSV-93-Ural'
+          ]
+        ]
+      },
+      2200: {
+        agenda: 'sm'
+      },
+      2300: {
+        agenda: 'sp',
+        operators: [
+          [
+            'player2',
+            'Captain-Shortspear'
+          ]
+        ]
+      }
+    },
+    player2: {
+      1800: {
+        agenda: 'pm',
+        operators: [
+          [
+            'player2',
+            'The-Gray-Baron'
+          ]
+        ]
+      },
+      1900: {
+        agenda: 'sm'
+      },
+      2000: {
+        agenda: 'ms',
+        operators: []
+      },
+      2100: {
+        agenda: 'ps',
+        operators: [
+          [
+            'player2',
+            'Mr-Archangel'
+          ], [
+            'player2',
+            'Dr-Archangel'
+          ], [
+            'player1',
+            'Soren-Glasskin'
+          ]
+        ]
+      },
+      2200: {
+        agenda: 'ps'
+      },
+      2300: {
+        agenda: 'mp'
+      }
+    },
+    outerworld: {
+      2100: {
+        hideouts: [
+          [
+            'player2',
+            'Avalon'
+          ]
+        ],
+        operators: [
+          [
+            'player2',
+            'Sourayah'
+          ]
+        ]
+      }
+    }
+  }
+}
